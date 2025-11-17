5bd9e048fd3ac93699720834e0c60d2afdade9e9
Rodo98Mendoza <Rodomendoza98@gmail.com>
2025-11-17 13:38:11 -0600

    Railway Setup done backend

--- mobile_app/src/constants.example.ts
+++ mobile_app/src/constants.example.ts
@@ -1,4 +1,4 @@
 // In mobile_app/src/constants.example.ts
 
 // TODO: Replace this with your computer's local IP address
-export const API_BASE_URL = 'http://192.168.1.178:8000/api';
+export const API_BASE_URL = 'respectful-flexibility-production.up.railway.app';
