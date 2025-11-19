e87479b72af8a148c73b1959af6606d11a8b9d22
Rodo98Mendoza <Rodomendoza98@gmail.com>
2025-11-19 12:17:41 -0600

    ERROR FOUND! I just forgot the https....

--- mobile_app/src/constants.example.ts
+++ mobile_app/src/constants.example.ts
@@ -1,4 +1,4 @@
 // In mobile_app/src/constants.example.ts
 
 // TODO: Replace this with your computer's local IP address
-export const API_BASE_URL = 'respectful-flexibility-production.up.railway.app';
+export const API_BASE_URL = 'https://respectful-flexibility-production.up.railway.app/api';
